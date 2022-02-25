2b8fb50979ad7dab3364abb9a840f1e57ede2578
Dong Liu <winter.solstice1219@gmail.com>
2022-02-25 07:46:30 -0600

    Install dev environment with some necessary packages

---
+++ contracts/NFTDrop.sol
@@ -0,0 +1,7 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "hardhat/console.sol";
