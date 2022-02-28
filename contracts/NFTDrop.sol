c39568186819faa163993265f17b10402bdaf29a
Dong Liu <winter.solstice1219@gmail.com>
2022-02-28 02:07:54 -0600

    Implement the nft drop contract as required

--- contracts/NFTDrop.sol
+++ contracts/NFTDrop.sol
@@ -6,10 +6,21 @@ import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "hardhat/console.sol";
 
-contract NFTDrop is ERC721Enumerable {
+contract NFTDrop is ERC721Enumerable, ReentrancyGuard, Ownable {
 
     ///@dev masterMinter
-    address public masterMinter;
+    address public preMinter;
+    /**
+     *@dev mint phases
+     *@param  pre pre minting phase
+     *@param WhiteList whitelist phase
+     *@param Public public minting phase
+     */
+    enum MintPhases {
+        Pre,
+        WhiteList,
+        Public
+    }
 
     /**
      * @dev token info
@@ -21,21 +32,39 @@ contract NFTDrop is ERC721Enumerable {
     struct TokenInfo {
         address minter;
         string name;
-        uint16 id;
         uint256 price;
     }
 
     ///@dev id => TokenInfo
-    mapping(uint16 => TokenInfo) tokenInfos;
+    mapping(uint256 => TokenInfo) tokenInfos;
 
-    ///@dev mint fee
-    uint256 public mintFee = 0.06 ether;
+    ///@dev address => bool
+    mapping(address => bool) whiteList;
+
+    ///@dev address => owned nft amount
+    mapping(address => uint256) ownedNFTs;
+    
+    ///@dev maximum amount
+    uint256 public maxAmount = 999;
+
+    ///@dev total supply
+    uint256 public tokenIdTracker;
 
     ///@dev baseTokenURI
     string public baseTokenURI;
 
+    ///@dev preMined flag
+    bool public preMinted;
+
+    ///@dev preMined flag
+    bool public whiteListFinished;
+
     ///@dev Mint event
-    event Minted(address minter, uint16 id);
+    event Minted(address minter, uint256 id);
+
+    event WhiteListAdded(address minter);
+
+    event WhiteListRemoved(address minter);
 
     /** 
      * @dev constructor
@@ -50,44 +79,112 @@ contract NFTDrop is ERC721Enumerable {
     )
         ERC721(_name, _symbol)
     {
-        masterMinter = _msgSender();
         setBaseURI(baseURI);
+        preMinter = _msgSender();
+    }
+
+    modifier saleIsOpen() {
+        require(tokenIdTracker <= maxAmount, "NFTDrop: maximum amount exceeded");
+        _;
     }
 
     /**
      * @dev mint card token to contract
-     * @param minter owner wallet address
      * @param name nft name
-     * @param id nft id
      * @param price nft price
+     * @param amount amount to be minted
      */
-    function mint(address minter, string calldata name, uint16 id, uint256 price) external payable {
-        require(minter != address(0), "NFTDrop: invalid minter address");
+    function mint(string calldata name, uint256 price, uint256 amount) external nonReentrant saleIsOpen {
         require(price > 0, "NFTDrop: invalid NFT price");
         require(bytes(name).length > 0, "NFTDrop: no NFT name");
-        require(tokenInfos[id].minter == address(0), "NFTDrop: already exists");
-        require(msg.value >= mintFee, "NFTDrop: not enough minting fee");
 
-        tokenInfos[id] = TokenInfo(
-            minter,
-            name,
-            id,
-            price
-        );
+        uint256 id = tokenIdTracker;
 
         //TODO premint #20, whitelist, public mint
 
+        if (id != 20 && id < 521 && preMinted) {        //whitelist phase
+            require(whiteList[_msgSender()], "NFTDrop: not whitelised address");
+            require(ownedNFTs[_msgSender()] == 0, "NFTDrop: already have a NFT minted");
+            require(amount == 1, "NFTDrop: not allowed mint amount");
 
-        _safeMint(minter, id);
-        emit Minted(minter, id);
+            if (id == 520) whiteListFinished = true;
+
+            tokenInfos[id] = TokenInfo(
+                _msgSender(),
+                name,
+                price
+            );
+            ownedNFTs[_msgSender()] += amount;
+            tokenIdTracker += amount;
+
+            _safeMint(_msgSender(), id);
+            emit Minted(_msgSender(), id);
+        } else if (id >= 521 && whiteListFinished) {             //public mint phase
+            require(ownedNFTs[_msgSender()] + amount <= 5, "NFTDrop: can mint 5 NFTs");
+
+            for (uint256 i = 0; i < amount; i++) {
+                tokenInfos[id] = TokenInfo(
+                    _msgSender(),
+                    name,
+                    price
+                );
+
+                _safeMint(_msgSender(), id);
+                emit Minted(_msgSender(), id);
+
+                id += 1;
+            }
+            ownedNFTs[_msgSender()] += amount;
+            tokenIdTracker += amount;
+        } else {                            //pre mint phase
+            require(_msgSender() == preMinter, "NFTDrop: not allowed preminter");
+            id = 20;
+            tokenInfos[id] = TokenInfo(
+                _msgSender(),
+                name,
+                price
+            );
+            preMinted = true;
+
+            _safeMint(_msgSender(), id);
+            emit Minted(_msgSender(), id);
+        }
 
     }
 
+    /**
+     * @dev  add minter addresses to whitelist
+     * @param minters array of minter wallet address
+     */
+    function addToWhiteList(address[] memory minters) external onlyOwner {
+        for (uint256 i = 0; i < minters.length; i ++) {
+            if (minters[i] != address(0)) whiteList[minters[i]] = true;
 
+            emit WhiteListAdded(minters[i]);
+        }
+    }
+
+    /**
+     * @dev  remove minter addresses from whitelist
+     * @param minters array of minter wallet address
+     */
+    function removeFromWhiteList(address[] memory minters) external onlyOwner {
+        for (uint256 i = 0; i < minters.length; i ++) {
+            if (minters[i] != address(0)) whiteList[minters[i]] = false;
+
+            emit WhiteListRemoved(minters[i]);
+        }
+    }
+
+    /**
+     * @dev set baseURI
+     * @param baseURI baseURI string
+     */
     function setBaseURI(string memory baseURI) public {
         baseTokenURI  = baseURI;
     }
 
+    ///@dev override _baseURI, return baseTokenURI
     function _baseURI() internal view virtual override returns (string memory) {
         return baseTokenURI;
     }
--- contracts/interfaces/INFTDrop.sol
+++ contracts/interfaces/INFTDrop.sol
@@ -7,27 +7,35 @@ interface INFTDrop {
      * @dev token info
      * @param minter nft owner wallet address
      * @param name nft name
-     * @param id nft id
      * @param price nft price
      */
     struct TokenInfo {
         address minter;
         string name;
-        uint16 id;
         uint256 price;
     }
 
     /**
      * @dev mint card token to contract
-     * @param minter owner wallet address
      * @param name nft name
-     * @param id nft id
      * @param price nft price
+     * @param amount amount of nfts to be minted
      */
     function mint(
-        address minter,
         string calldata name,
-        uint16 id,
-        uint256 price
+        uint256 price,
+        uint256 amount
     ) external payable;
+
+    /**
+     * @dev  add minter address to whitelist
+     * @param minters array of minter wallet addresses
+     */
+    function addToWhitelist(address[] memory minters) external;
+
+    /**
+     * @dev  remove minter address from whitelist
+     * @param minters array of minter wallet address
+     */
+    function removeFromWhiteList(address[] memory minters) external;
 }
